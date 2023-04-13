1afa6db6e2d4938f15a18baaa80f342f40a20fdc
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-04-13 12:32:59 +0600

    sort change in mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(express.json());
 //igkXRxzSyJwIqLFs
 
 const uri = "mongodb+srv://organizationManager:igkXRxzSyJwIqLFs@cluster0.qogqlqn.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', function (req, res) {
     res.json({ msg: 'Organization Manager ' })
