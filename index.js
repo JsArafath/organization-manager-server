3b0d09024482aa22b2740f4ae035faf48dc512b3
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-04-14 16:07:07 +0600

    Database connection changed

--- index.js
+++ index.js
@@ -18,11 +18,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//organizationManager
-//igkXRxzSyJwIqLFsJ
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qogqlqn.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d0mpncw.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect((err) => {
